0cd7aba7ce56acaa4cd9d9d952742fc7ecd433bc
Flammable-Duck <flammableduck@protonmail.com>
2021-02-12 22:20:15 -0800

    first commit

---
+++ _drafts/newpost.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import subprocess
+from datetime import datetime
+now = datetime.now()
+
+print("New Post Name:")
+title= input()
+title= title.title()
+formatTitle= title.replace(" ","-")
+formatTitle=formatTitle.lower()
+fileName=now.strftime("%Y-%m-%d")+ "-" + formatTitle + ".md"
+
+f = open(fileName, "w")
+f.write('---\nlayout: default\ntitle: "'+title+'"\n---\n')
+f.close
